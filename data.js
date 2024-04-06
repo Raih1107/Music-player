e7fcd5e35f54a74b3c85295207212a29ba14d7a3
Himanshu Rai <raih68737@gmail.com>
2024-04-07 00:25:02 +0530

    Add

---
+++ data.js
@@ -0,0 +1,117 @@
+let songs = [
+    {
+        name: 'Euphoria',
+        path: 'assets/musics/Euphoria.mp3',
+        artist: 'Narvent',
+        cover: 'assets/images/image(1).jpg'
+    },
+    {
+        name: 'Fainted',
+        path: 'assets/musics/Fainted.mp3',
+        artist: 'Narvent',
+        cover: 'assets/images/images (2).jpeg'
+    },
+    {
+        name: 'Rubicon Drill',
+        path: 'assets/musics/Rubicon Drill.mp3',
+        artist: 'Laddi Chahal',
+        cover: 'assets/images/images (3).jpeg'
+    },
+    {
+        name: 'Admirin You',
+        path: 'assets/musics/Admirin_You.mp3',
+        artist: 'Karan Aujla',
+        cover: 'assets/images/images (4).jpeg'
+    },
+    {
+        name: 'Check It Out',
+        path: 'assets/musics/Check It Out.mp3',
+        artist: 'Parmish Verma',
+        cover: 'assets/images/images (5).jpeg'
+    },
+    {
+        name: 'Kaana Pe Baal',
+        path: 'assets/musics/Kaana Pe Baal Feat. Pranjal Dahiya.mp3',
+        artist: 'Amanraj Gill',
+        cover: 'assets/images/images (6).jpeg'
+    },
+    {
+        name: 'Blue Monday',
+        path: 'assets/musics/Blue Monday (2020 Digital Master).mp3',
+        artist: 'New Order',
+        cover: 'assets/images/images (7).jpeg'
+    },
+    {
+        name: 'Midnight City',
+        path: 'assets/musics/Midnight City.mp3',
+        artist: 'M83',
+        cover: 'assets/images/images (8).jpeg'
+    },
+    {
+        name: 'Popular',
+        path: 'assets/musics/Popular_The_Idol_Vol_1_Music_from_the_HBO_Original_Series.mp3',
+        artist: 'The Weeknd',
+        cover: 'assets/images/images (9).jpeg'
+    },
+    {
+        name: 'Living Life, In The Night',
+        path: 'assets/musics/Living Life, In The Night (slowed).mp3',
+        artist: 'Cheriimoya',
+        cover: 'assets/images/images (10).jpeg'
+    },
+    {
+        name: 'After Dark',
+        path: 'assets/musics/After Dark.mp3',
+        artist: 'Mr.Kittty',
+        cover: 'assets/images/images (11).jpeg'
+    },
+    {
+        name: 'Night Work',
+        path: 'assets/musics/Night Work.mp3',
+        artist: 'Sione',
+        cover: 'assets/images/images (12).jpeg'
+    },
+    {
+        name: 'Night Call',
+        path: 'assets/musics/Nightcall.mp3',
+        artist: 'Kavinsky',
+        cover: 'assets/images/images (13).jpeg'
+    },
+    {
+        name: 'Wake Up',
+        path: 'assets/musics/WAKE UP!.mp3',
+        artist: 'Moondiety',
+        cover: 'assets/images/images (14).jpeg'
+    },
+    {
+        name: 'METAMORPHOSIS',
+        path: 'assets/musics/METAMORPHOSIS.mp3',
+        artist: 'INTERWORLD',
+        cover: 'assets/images/images (15).jpeg'
+    },
+    {
+        name: 'Sleepwalker',
+        path: 'assets/musics/Sleepwalker (Slowed).mp3',
+        artist: 'LONOWN,',
+        cover: 'assets/images/images (16).jpeg'
+    },
+ 
+    {
+        name: 'Neonblade',
+        path: 'assets/musics/NEON BLADE.mp3',
+        artist: 'Moondiety',
+        cover: 'assets/images/images (18).jpeg'
+    },
+    {
+        name: 'ecstasy',
+        path: 'assets/musics/ecstacy (slowed).mp3',
+        artist: 'SUICIDAL-IDOL ',
+        cover: 'assets/images/images (19).jpeg'
+    },
+    {
+        name: 'Killers Form The Northside',
+        path: 'assets/musics/Killers From The Northside.mp3',
+        artist: 'Kordhell',
+        cover: 'assets/images/images (20).jpeg'
+    }
+]
